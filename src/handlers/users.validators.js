fb8ef952f7f350452d53fb7b171f00c0d98822c7
v-potap <v.o.potapenko@gmail.com>
2019-10-10 17:15:01 +0300

    hw-01 v1.0.0

---
+++ src/handlers/users.validators.js
@@ -0,0 +1,24 @@
+class UsersValidators {
+  static validateSignUpUser(body) {
+    const response = {
+      status: "",
+      user: {}
+    };
+
+    if (!body) {
+      response.status = "Error: invalid request";
+      return { errCode: 400, response };
+    }
+
+    response.user = JSON.parse(body);
+
+    if (!response.user.username) {
+        response.status="Error: invalid request => 'username' is required"
+        return { errCode: 400, response };
+      }
+
+    return undefined;
+  }
+}
+
+module.exports = UsersValidators;
---
+++ src/server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const url = require("url");
+
+const morgan = require("morgan");
+
+const RouteHandlers = require("./handlers/route.handlers");
+
+const logger = morgan("combined");
+
+const startServer = port => {
+  const server = http.createServer((req, res) => {
+    // Get route from the request
+    const route = url.parse(req.url).pathname;
+
+    // Get router function
+    const func = RouteHandlers.getHandler(route, req.method);
+
+    logger(req, res, () => func(req, res));
+  });
+
+  server.listen(port);
+};
+
+module.exports = startServer;
