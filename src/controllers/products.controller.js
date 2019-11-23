5c7b85c8319d572c2378b2f1431baba363b4d9c5
v-potap <v.o.potapenko@gmail.com>
2019-11-23 13:15:02 +0200

    hw-05 v0.2.0

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -9,31 +9,29 @@ class ProductsController {
     const productIDs = qs.parse(url.parse(req.url).query)["ids"];
     const categoryIDs = qs.parse(url.parse(req.url).query)["category"];
 
-    const products = productIDs ? productIDs.split(",") : [];
-    const categories = categoryIDs ? categoryIDs.split(",") : [];
-
-    const allProducts = await Product.find()
-
-    debugger;
+    const products = productIDs ? productIDs.replace(/\"/g, "").split(",") : [];
+    const categories = categoryIDs
+      ? categoryIDs.replace(/\"/g, "").split(",")
+      : [];
 
     let filteredProducts = [];
+    let criteria = {};
 
     if (products.length > 0) {
-      filteredProducts = allProducts.filter(product =>
-        products.includes(product.id + "")
-      );
-    } else {
-      filteredProducts = [...allProducts];
+      criteria._id = { $in: products };
     }
 
     if (categories.length > 0) {
-      filteredProducts = filteredProducts.filter(product => {
-        const productsOfCategory = product.categories.filter(category =>
-          categories.includes(category)
-        );
-        return productsOfCategory.length > 0;
-      });
+      criteria.categories = { $in: categories };
     }
+
+    if (criteria._id && criteria.categories) {
+      criteria = {
+        $and: [{ _id: criteria._id }, { categories: criteria.categories }]
+      };
+    }
+
+    filteredProducts = await Product.find(criteria);
 
     if (filteredProducts.length > 0) {
       res.writeHead(200, "OK", {
@@ -52,27 +50,21 @@ class ProductsController {
     }
   }
 
-  static getProductsByID(req, res) {
-    const productsPath = path.join(
-      __dirname,
-      "../db/products",
-      "all-products.json"
-    );
+  static async getProductsByID(req, res) {
     const route = url.parse(req.url).pathname;
     const productID = Number.parseInt(route.slice(1));
-    const allProducts = JSON.parse(fs.readFileSync(productsPath));
-    const products = allProducts.filter(product => product.id === productID);
+    const product = await Product.findOne({ _id: productID });
 
-    if (products.length > 0) {
+    if (product) {
       res.writeHead(200, "OK", {
         "Content-Type": "application/json"
       });
-      res.end(JSON.stringify({ status: "success", products }));
+      res.end(JSON.stringify({ status: "success", products: [product] }));
     } else {
       res.writeHead(400, "Error: invalid request", {
         "Content-Type": "application/json"
       });
-      res.end(JSON.stringify({ status: "no products", products }));
+      res.end(JSON.stringify({ status: "no products", products: [] }));
     }
   }
 }
